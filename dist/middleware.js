33c5eca100a2a0d029d900fb7fd403e68f368816
masatokai1212 <ishome.kai@gmail.com>
2024-12-06 11:04:32 +0900

    不要なファイルの削除と出力ディレクトリの変更

--- dist/middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const middleware = (config) => {
-    return (req, res, next) => {
-        // ミドルウェアのロジック
-        next();
-    };
-};
-exports.default = middleware;
